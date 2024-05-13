8b3d0d9ceab8b16702187c304913684ac50a8e30
YuriyMitrofanov <yurasell@yandex.ru>
2024-05-13 21:48:49 +0300

    implemented scaling photos at the articleCardLarge

--- src/app/mocData/articles.js
+++ src/app/mocData/articles.js
@@ -61,5 +61,68 @@ export const articlesList = [
         img: "https://sun9-24.userapi.com/impf/c637319/v637319313/e854/yLIWdBqqrJI.jpg?size=1280x781&quality=96&sign=b1366a370cae1d338e80e96a9db613e5&c_uniq_tag=SZmVhNHyL7ff-7z3oFOuJ8m20O6qWkqYcEVafus3xz8&type=album",
         content: "Содержание статьи 9",
         title: "title 1"
+    },
+    {
+        id: "10",
+        header: "Название статьи 10",
+        img: "https://i.pinimg.com/originals/f4/24/1e/f4241e3b0379c16b1985bbaf1c26a336.jpg",
+        content: "Содержание статьи 10",
+        title: "title 1"
+    },
+    {
+        id: "11",
+        header: "Название статьи 11",
+        img: "https://f.vividscreen.info/soft/1b166db15b3f5ca4a524bbd9979cc812/Starry-Space-1280x960.jpg",
+        content: "Содержание статьи 11",
+        title: "title 3"
+    },
+    {
+        id: "12",
+        header: "Название статьи 12",
+        img: "https://kartinki.pics/pics/uploads/posts/2022-09/thumbs/1662158263_61-kartinkin-net-p-kosmicheskaya-poverkhnost-instagram-65.jpg",
+        content: "Содержание статьи 12",
+        title: "title 2"
+    },
+    {
+        id: "13",
+        header: "Название статьи 13",
+        img: "https://catherineasquithgallery.com/uploads/posts/2023-01/1674552407_catherineasquithgallery-com-p-kosmos-korichnevii-fon-177.jpg",
+        content: "Содержание статьи 13",
+        title: "title 5"
+    },
+    {
+        id: "14",
+        header: "Название статьи 14",
+        img: "https://i.ytimg.com/vi/djysULthnj8/maxresdefault.jpg",
+        content: "Содержание статьи 14",
+        title: "title 2"
+    },
+    {
+        id: "15",
+        header: "Название статьи 15",
+        img: "https://wallpaperaccess.com/full/487687.jpg",
+        content: "Содержание статьи 15",
+        title: "title 3"
+    },
+    {
+        id: "16",
+        header: "Название статьи 16",
+        img: "https://img.goodfon.ru/original/1600x1200/b/6c/galaktika-kosmos-zvezdy.jpg",
+        content: "Содержание статьи 16",
+        title: "title 3"
+    },
+    {
+        id: "17",
+        header: "Название статьи 17",
+        img: "https://c.wallhere.com/photos/79/76/space_TylerCreatesWorlds_space_art_digital_art_artwork_stars_nebula-121371.jpg!d",
+        content: "Содержание статьи 17",
+        title: "title 4"
+    },
+    {
+        id: "18",
+        header: "Название статьи 18",
+        img: "https://sun9-24.userapi.com/impf/c637319/v637319313/e854/yLIWdBqqrJI.jpg?size=1280x781&quality=96&sign=b1366a370cae1d338e80e96a9db613e5&c_uniq_tag=SZmVhNHyL7ff-7z3oFOuJ8m20O6qWkqYcEVafus3xz8&type=album",
+        content: "Содержание статьи 18",
+        title: "title 1"
     }
 ];
